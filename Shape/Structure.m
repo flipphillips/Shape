849dc6094bdfecfe0a3f205d48b2d884dff18c59
flipphillips <flip@skidmore.edu>
2016-12-09 21:12:39 -0500

    object bulk local processing

--- Shape/Structure.m
+++ Shape/Structure.m
@@ -14,10 +14,15 @@
 BeginPackage["Structure`"]
 (* Exported symbols added here with SymbolName::usage *)  
 VertexFrame::usage="VertexFrame[v,n,neighborhood] calculates the vertex frame for vertex v with normal n and neighbors."
-
+OBJStructure::usage="OBJStructure[filename] calculates the differential structure of the OBJ file. This file should have vertex normals."
+ 
 Begin["`Private`"] (* Begin Private Context *) 
 
-VertexFrame[v_,n_,hood_]:=Module[{nhood,xhood,m,a,b,c,ks,pd,pds},
+(*
+ * returns p,n,pd1,pd2,k1,k2
+ *)
+
+VertexFrame[v_,n_,hood_]:=Module[{nhood,xhood,m,a,b,c,ks,pd,pds,pd1,pd2},
 	(* degenerate cases *)
 	If[Chop[Norm[n],10^-8]==0||Chop[Norm[n-{0.,0.,-1.}],10^-8]==0,
 		Return[{{0.,0.},{{0.,0.,0.},{0.,0.,0.}}}]];
@@ -36,30 +41,39 @@ VertexFrame[v_,n_,hood_]:=Module[{nhood,xhood,m,a,b,c,ks,pd,pds},
 	{a,b,c}=LeastSquares[m,xhood[[All,3]]];
 	{ks,pd}=Eigensystem[{{a,b},{b,c}}];
 
+	(* rotate them back *)
 	pds=If[Chop[n]!={0,0,1.},
 		RotationTransform[{{0.,0.,1.},n}]/@(Append[#,0.]&/@pd),
 		pd];
 
-	If[LessEqual@@ks,
-		{ks,pds},Reverse/@{ks,pds}]]
+	(* sort them from min,max *)
+	{{pd1,pd2},ks}=If[LessEqual@@ks,
+		{pds,ks},Reverse/@{pds,ks}];
+
+	{v,n,pd1,pd2,ks}
+]
+
+neighborFest[i_,raf_,ri_]:=Select[Union[Flatten[Nest[raf, i, ri]]], # != i &]
+
+(*raf = ReplaceAll[Dispatch[mo["VertexVertexConnectivityRules"]]];*)
+  
 
 
-MeshRegionFrames[o_, ns_] := 
- Module[{mo, ps, vcr, vvc, vvcr, raf, hoods, neighbors},
+OBJStructure[f_] := 
+ Module[{o,ns,mo,ps,vcr,vvc,hoods},
+  o = Import[f,"MeshRegion"];
+  ns = Normalize/@Import[f,"VertexNormals"];
+  
   mo = Graphics`Region`ToMeshObject[o];
   
   ps = mo["VertexCoordinates"];
   vcr = Dispatch[mo["VertexCoordinateRules"]];
   vvc = mo["VertexVertexConnectivity"];
-  vvcr = Dispatch[mo["VertexVertexConnectivityRules"]];
-  
-  raf = ReplaceAll[vvcr];
-  neighbors[i_, ri_] := 
-   Select[Union[Flatten[Nest[raf, i, ri]]], # != i &];
   
   hoods = vvc /. vcr;
   
-  MapThread[VertexFrame, {ps, ns, hoods}]]
+  MapThread[VertexFrame, {ps, ns, hoods}]
+]
   		
 End[] (* End Private Context *)
 
